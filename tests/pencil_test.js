1ed096d20c41124da1e1407f729ab93c8aaa7255
someone <someone>
2019-10-18 10:14:11 -0500

    clean up tests, setting paper empty for each test

--- tests/pencil_test.js
+++ tests/pencil_test.js
@@ -4,25 +4,27 @@ var Pencil = require('../pencil.js');
 var Paper = require('../paper.js');
 
 describe('PencilTests', function() {
+    beforeEach( function() {
+        Paper.set_contents('');
+    })
 
     it('handle degradation of pencil with lower case characters', function() {
-        Paper.set_contents('');
         Pencil.set_durability(4);
         Paper.set_contents(Paper.get_contents() + Pencil.write('test'));
         expect(Paper.get_contents()).to.eql('test');
     });
 
     it('handle degradation of pencil with upper case characters', function() {
-        Paper.set_contents('');
         Pencil.set_durability(4);
         Paper.set_contents(Paper.get_contents() + Pencil.write('Test'));
         expect(Paper.get_contents()).to.eql('Tes');
     });
 
     it('handle degradation of pencil with space or return characters', function() {
-        Paper.set_contents('');
         Pencil.set_durability(16);
         Paper.set_contents(Paper.get_contents() + Pencil.write('Test Green\r\nRefactor'));
         expect(Paper.get_contents()).to.eql('Test Green\r\nRefa');
     });
+
+
 });
