0c070f7b7ace6f874d05fe4adf6ed4e91ccc866f
someone <someone>
2019-10-18 09:46:25 -0500

    add in functionality to handle removing 2 durability points for upper case characters

--- pencil.js
+++ pencil.js
@@ -12,9 +12,13 @@ class Pencil {
         let write_characters = '';
         let character_interator = 0;
         while (this.durability > 0 && character_interator < characters.length) {
+            let written_characters = 1;
+            if(characters[character_interator].search(/[A-Z]/) == 0) {
+                written_characters = 2;
+            }
             write_characters += characters[character_interator];
             character_interator++;
-            this.durability--;
+            this.durability = this.durability - written_characters;
         }
         return write_characters;
     }
--- tests/pencil_test.js
+++ tests/pencil_test.js
@@ -11,4 +11,11 @@ describe('PencilTests', function() {
         Paper.set_contents(Paper.get_contents() + Pencil.write('test'));
         expect(Paper.get_contents()).to.eql('test');
     });
+
+    it('handle degradation of pencil with lower case characters', function() {
+        Paper.set_contents('');
+        Pencil.set_durability(4);
+        Paper.set_contents(Paper.get_contents() + Pencil.write('Test'));
+        expect(Paper.get_contents()).to.eql('Tes');
+    });
 });
